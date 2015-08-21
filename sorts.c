7e86277a7f8f1008e876bded92f5137f38c20edd
David Ferguson <deejai@live.com>
2015-08-21 14:37:51 -0400

    fwd declaration of print_ints

--- sorts.c
+++ sorts.c
@@ -2,6 +2,11 @@
 #include <stdlib.h>
 #include <string.h>
 
+// Forward declaration of print function that each sort
+// makes use of
+void print_ints( int *nums, int length, int partition_a,
+                                        int partition_b );
+
 #include "sort_insertion.h"
 //#include "sort_selection.h"
 #include "sort_bubble.h"
@@ -25,6 +30,24 @@ void demo_sort( void (*function)(int*,int), int *nums, int length )
 
     // free copied array
     free( nums_copy );
+}
+
+void print_ints( int *nums, int length, int partition_a,
+                                        int partition_b )
+{
+    // Print the given integer array along with up to two
+    // partition indicators
+
+    printf(" ");
+
+    int i;
+    for( i=0; i<length; i++ ){
+        printf("%d ", nums[i]);
+        if( i==partition_a || i==partition_b )
+            printf("| ");
+    }
+
+    printf("\n");
 }
 
 int main( int argc, char *argv[] )
